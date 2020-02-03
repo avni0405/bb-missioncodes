3f3392b1932c96ac6fb157e0754e962d066a878f
avni0405 <avinavekariya6@gmail.com>
2020-02-03 22:09:12 +0530

    design chanfe

---
+++ aboutus.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">   
+    <?php include 'header.php' ?>
+        <!-- top bar -->
+        <section class="l-section wpb_row height_medium color_secondary home_bg with_img with_overlay ult-vc-hide-row upb_video_class uvc-row" id="home" style="position: relative;" data-rtl="false" data-row-effect-mobile-disable="true" data-upb_br_animation="">
+            <div class="ult-vc-seperator bottom_seperator ult-xlarge-triangle uvc-seperator-4962811336876" data-full-width="true" data-border="undefined" data-border-width="undefined">
+                <div class="ult-main-seperator-inner">
+                    <svg class="uvc-x-large-triangle" xmlns="http://www.w3.org/2000/svg" version="1.1" fill="#ffffff" width="100%" height="140" viewBox="0 0 4.66666 0.333331" preserveAspectRatio="none" style="height: 140px;">
+                        <path class="fil0" d="M-0 0.333331l4.66666 0 0 -3.93701e-006 -2.33333 0 -2.33333 0 0 3.93701e-006zm0 -0.333331l4.66666 0 0 0.166661 -4.66666 0 0 -0.166661zm4.66666 0.332618l0 -0.165953 -4.66666 0 0 0.165953 1.16162 -0.0826181 1.17171 -0.0833228 1.17171 0.0833228 1.16162 0.0826181z"></path>
+                    </svg>
+                </div>
+                <div class="separator-icon"></div>
+            </div>
+            <div class="upb_video-wrapper">
+                <div class="upb_video-bg" data-bg-override="0" data-rtl="false" data-row="wpb_row" data-theme-support="" style="width: 343px; min-width: 343px; left: 0px;">
+                    <video class="upb_video-src enable-on-viewport hosted-video" preload="auto">
+                        <source type="video/mp4" src="http://192.168.0.100/wordpress2/wp-content/uploads/2019/02/WhatsApp-Video-2019-02-27-at-9.37.24-PM.mp4">
+                    </video>
+                </div>
+            </div>
+            <div class="l-section-img loaded" style="background-image: url(image/top.jpeg);" data-img-width="5184" data-img-height="4000"></div>
+            <div class="l-section-overlay" style="background-color: rgba(0,0,0,0.56)"></div>
+            <div class="l-section-h i-cf">
+                <div class="g-cols vc_row type_default valign_top">
+                    <div class="vc_col-sm-12 wpb_column vc_column_container align_center">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class="ult-spacer spacer-5e2725bd21cac" data-id="5e2725bd21cac" data-height="100" data-height-mobile="50" data-height-tab="50" data-height-tab-portrait="50" data-height-mobile-landscape="50" style="clear:both;display:block;">
+                                </div>
+                                <h1 style="font-size: 2.5rem;color: #ffffff;line-height: 1.1;text-align: center;margin-bottom: 50px;;" class="vc_custom_heading">About Us</h1>
+                                <div class="w-separator size_custom" style="height:500;"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- booking -->
+        <div class="bckimg" style="margin-bottom:0px;">
+            <div class="container" style="padding-bottom:150px;">
+                <div class="row" style="padding-top:50px;">
+                    <div class="col-md-6">
+                        <img src="image/about.png" width="500px" style="overflow:hidden;">
+                    </div>
+                    <div class="col-md-6">
+                        <p style="text-align: center;padding-top:45px;padding-bottom: 40px;"><p>Summer means happy times and good sunshine!! An </p><p>all  new project on the outskirts of the diamond city-</p><p>Surat which will surely amuse you . Blue bubble water</p>water park, a place filled with a lot of fun and excitement.</p><p> Waterslides, fun rides, pools with original games for</p><p> all ages, and for real endless summer fun and get real</p><p> summer experience for entire family. We don’t create</p><p> a fantasy world to escape reality, we create it to be </p><p>able to stay..!!</p>
+                            <p>So, climb to the peak of summer bliss with the fun </p><p>options of water park.</p></p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <p style="padding-bottom:100px;font-size:25px;font-weight:bold;text-align:center;text-decoration:underline;">
+Here are the three little things that makes us special
+            </p>
+            <div class="row">
+                <div class="col-md-4">
+                <p style="font-size:23px;font-weight:bold;"><i class="fas fa-snowboarding about-icon"></i>
+                    Unlimited Fun</p>
+                </div>
+                <div class="col-md-4">
+                <p style="font-size:23px;font-weight:bold;"><i class=" fas fa-hamburger about-icon"></i>
+                    Delicious Food</p>
+                </div>
+                <div class="col-md-4">
+                <p style="font-size:23px;font-weight:bold;"><i class="fas fa-atom about-icon"></i>
+                    Unforgottble Memories</p>
+                </div>
+            </div>
+            </div>
+        </div>
+        <?php include 'footer.php' ?>
+    </body>
+</html>
